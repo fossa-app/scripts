d328dfc5f4bf03a25a02003f85ca755776d4690f
Tigran TIKSN Torosyan <personal@tiksn.com>
2023-07-31 21:58:37 +0200

    Create Setup.ps1

---
+++ Setup.ps1
@@ -0,0 +1,11 @@
+Install-Module -Name Microsoft.PowerShell.SecretManagement -Force
+Install-Module -Name Microsoft.PowerShell.SecretStore -Force
+Install-Module -Name SecretManagement.Keybase -Force
+
+if (-not (Get-SecretVault -Name SecretStore -ErrorAction SilentlyContinue)) {
+    Register-SecretVault -Name SecretStore -ModuleName Microsoft.PowerShell.SecretStore -DefaultVault
+}
+
+if (-not (Get-SecretVault -Name FossaApp -ErrorAction SilentlyContinue)) {
+    Register-KeybaseSecretVault -Name FossaApp -Namespace FossaApp -Team fossaapp
+}
