679ccd08f91fc585733b2466b48a7ee461b8d510
Tigran TIKSN Torosyan <personal@tiksn.com>
2026-02-20 14:57:36 -0600

    feat: update Sentry OpenTelemetry integration to use OTLP HTTP exporter with SENTRY_KEY.

--- .build.ps1
+++ .build.ps1
@@ -19,7 +19,7 @@ task CreateDotEnv -If { -not (Test-Path -Path '.env') } {
     $connectionString = Get-Secret -Name 'FossaApp-ConnectionString' -AsPlainText
     $datadogApiKey = Get-Secret -Name 'FossaApp-DatadogApiKey' -AsPlainText
     $honeycombApiKey = Get-Secret -Name 'FossaApp-HoneycombApiKey' -AsPlainText
-    $sentryDsn = Get-Secret -Name 'FossaApp-SentryDsn' -AsPlainText
+    $sentryKey = Get-Secret -Name 'FossaApp-SentryKey' -AsPlainText
 
     $generatorId = Get-Random -Maximum 1024
 
@@ -31,7 +31,7 @@ task CreateDotEnv -If { -not (Test-Path -Path '.env') } {
         'DD_API_KEY'                 = $datadogApiKey
         'HONEYCOMB_API_KEY'          = $honeycombApiKey
         'HONEYCOMB_DATASET'          = 'FossaApp-Local'
-        'SENTRY_DSN'                 = $sentryDsn
+        'SENTRY_KEY'                 = $sentryKey
         'Messaging__Topic'           = 'test'
     }
 
