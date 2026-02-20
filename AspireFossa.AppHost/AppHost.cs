acdfe29e8762669e9b32550bfb19bd1162851b3d
Tigran TIKSN Torosyan <personal@tiksn.com>
2026-02-20 15:25:12 -0600

    feat: Add initial Aspire AppHost project to orchestrate API and PowerShell components.

--- AspireFossa.AppHost/AppHost.cs
+++ AspireFossa.AppHost/AppHost.cs
@@ -1,10 +1,17 @@
 using CommunityToolkit.Aspire.Hosting.PowerShell;
 using System.Management.Automation;
 using DotNetEnv;
+using System.IO;
+using System.Text.Json.Nodes;
 
 var builder = DistributedApplication.CreateBuilder(args);
 
 var envVars = Env.TraversePath().Load();
+
+var kickstartPath = Path.Combine(builder.Environment.ContentRootPath, "..", "kickstart", "kickstart.json");
+var kickstartJson = await File.ReadAllTextAsync(kickstartPath);
+var kickstartNode = JsonNode.Parse(kickstartJson);
+var apiKey = kickstartNode?["variables"]?["apiKey"]?.GetValue<string>() ?? "047a124c-2dbc-4b68-84cc-55f9a204f4ea";
 
 var ps = builder.AddPowerShell("ps", PSLanguageMode.FullLanguage);
 
@@ -14,6 +21,8 @@ var startScript = ps.AddScript("Start", """
     .WithArgs();
 
 var api = builder.AddProject<Projects.API_Web>("api")
+    .WithEnvironment("Identity__RootAddress", "http://localhost:9011/")
+    .WithEnvironment("Identity__ApiKey", apiKey)
     .WithHttpHealthCheck("/healthchecks")
     .WithExternalHttpEndpoints()
     .WaitFor(ps)
